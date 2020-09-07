5a49cf546e0df05f97d62d37b5ec4a9eb451cf7e
suyash shrivastava <suyash2796@gmail.com>
2020-09-07 20:33:53 +0530

    excel resource added

--- src/certificate_generator.py
+++ src/certificate_generator.py
@@ -1,11 +1,17 @@
 from PIL import Image, ImageFont, ImageDraw
-im = Image.open("../data/incoming/test.png")
-d = ImageDraw.Draw(im)
+import pandas as pd
 
+df = pd.read_excel("../data/resource/test_names.xlsx")
+names = df['NAMES'].to_list()
+im = Image.open("../data/incoming/test.png")
 location = (220, 60)
 text_color = (100, 100, 200)
 font_path = "../data/resource/arial.ttf"
 font = ImageFont.truetype(font_path, 26)
-d.text(location, "suyash shrivastava",fill=text_color, font=font)
-
-im.save("../data/work/certificate.png")
+for name in names:
+	im = Image.open("../data/incoming/test.png")
+	d = ImageDraw.Draw(im)	
+	d.text(location, name,fill=text_color, font=font)
+	certificate_name = "../data/work/"+ name + ".png"
+	im.save(certificate_name)
+	print("Certificate generated for-{}".format(name))
